c502ef7d465f21f7a0197c952b53e59ed20c5bdc
sameerasad <ssameerkhan525@gmail.com>
2020-07-07 23:54:41 -0400

    add header componet

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const header = () => {
+    return (
+        <div>
+            <h3>
+                  CI/CD PIPELINE WORKS HURRAH!
+            </h3>
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import header from './header';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <header />
   </React.StrictMode>,
   document.getElementById('root')
 );
